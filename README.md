e208cc1e48694dbee2a35008cfcf7cccef664264
Andrey Rybalko <root.andriell@gmail.com>
2020-11-23 17:21:37 +0300

    v1.0.0a

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# wemos-casket
+# Wemos Casket
+
+## How to flash
+After downloading this source, rename config.ino.example to config.ino and write your options there.
--- wemos-casket.ino
+++ wemos-casket.ino
@@ -3,6 +3,31 @@
 // GxEPD2
 // GFX_Root
 
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "const.h"
+// ArduinoJson
+#include <ArduinoJson.h>
+// Time Timekeeping functionality for Arduino
+#include <TimeLib.h>
+
+#define WEMOS_D0     16
+#define WEMOS_D1     5
+#define WEMOS_D2     4
+#define WEMOS_D3     0
+#define WEMOS_D4_LED 2
+#define WEMOS_D5     14
+#define WEMOS_D6     12
+#define WEMOS_D7     13
+#define WEMOS_D8     15
+#define WEMOS_TX     1
+#define WEMOS_RX     3
+
+#define TFT_CS   WEMOS_D2
+#define TFT_DC   WEMOS_D3
+#define TFT_RST  WEMOS_D1
+
+
 // VCC  - 3v3
 // GND  - GND
 // D\C  - D3
